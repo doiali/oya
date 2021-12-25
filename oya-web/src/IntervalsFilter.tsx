d6c175f37c088124ceaf776fe4552ea40e03a79a
abagheri1994 <abagheri1994@gmail.com>
2021-12-26 00:56:56 +0330

    added useIntervalsFilter

--- oya-web/src/IntervalsFilter.tsx
+++ oya-web/src/IntervalsFilter.tsx
@@ -1,14 +1,14 @@
 import { Autocomplete, Box, Button, Collapse, Stack, TextField, Typography } from '@mui/material';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Activity, Interval } from './apiService';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import { DateTimePicker } from '@mui/lab';
 import AdapterJalali from '@date-io/date-fns-jalali';
 
 type IntervalsFilterProps = {
-  intervals: Interval[],
+  // intervals: Interval[],
   activities: Activity[],
-  filteredIntervals: Interval[],
+  // filteredIntervals: Interval[],
   state: {
     selectedActivities: Activity[],
     start: Date,
@@ -16,9 +16,9 @@ type IntervalsFilterProps = {
   },
   results: {
     sum: number,
-    occurance: number,
-    avgPerDay: number,
-    days: number,
+    // occurance: number,
+    // avgPerDay: number,
+    // days: number,
   },
   onChange(name: 'start' | 'end', value: Date | null): void,
   onChange(name: 'selectedActivities', value: Activity[]): void,
@@ -86,4 +86,50 @@ export default function IntervalsFilter({
       </Collapse>
     </Box>
   );
+}
+
+/**
+ * checks if a is b or a child of b
+ * @param a child activity
+ * @param b parent activity
+ */
+export const isChildOf = (a: Activity, b: Activity): boolean => {
+  if (a.id === b.id) return true;
+  return a.parents.some(p => isChildOf(p, b));
+};
+
+type useIntervalsFilterProps = {
+  intervals: Interval[],
+  // activities: Activity[],
+};
+
+export function useIntervalsFilter({ intervals }: useIntervalsFilterProps) {
+  const [state, setState] = useState(() => {
+    let min = new Date().toISOString();
+    intervals.forEach(i => {
+      if (i.start < min) min = i.start;
+    });
+    return {
+      start: new Date(min),
+      end: intervals[0]?.end ? new Date(intervals[0].end) : new Date(),
+      selectedActivities: [] as Activity[],
+    };
+  });
+
+  const onChange: IntervalsFilterProps['onChange'] = (name, value) => {
+    setState(prev => ({ ...prev, [name]: value }));
+  };
+
+  const filteredIntervals = useMemo(() => {
+    return intervals.filter(i => (
+      (i.start >= state.start.toISOString() || i.end <= state.end.toISOString()) &&
+      i.entries.map(e => e.activity).some(a => state.selectedActivities.some(b => isChildOf(a, b)))
+    ));
+  }, [state, intervals]);
+
+  const results = useMemo(() => ({
+    sum: filteredIntervals.reduce((a, v) => (new Date(v.start).getTime() - new Date(v.end).getTime()) / 60000 + a, 0),
+  }), [filteredIntervals]);
+
+  return { state, onChange, filteredIntervals, results };
 }
