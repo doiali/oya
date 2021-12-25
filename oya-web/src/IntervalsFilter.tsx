9529df6a6605bbcaa57b3fce77c6da3e00a9f08f
abagheri1994 <abagheri1994@gmail.com>
2021-12-26 01:57:27 +0330

    web: added reset button to Interval filters

--- oya-web/src/IntervalsFilter.tsx
+++ oya-web/src/IntervalsFilter.tsx
@@ -1,9 +1,10 @@
-import { Autocomplete, Box, emphasize, Paper, Stack, TextField, Typography, useTheme } from '@mui/material';
-import { useMemo, useState } from 'react';
+import { Autocomplete, Box, Button, emphasize, Paper, Stack, TextField, Typography, useTheme } from '@mui/material';
+import { useMemo } from 'react';
 import { Activity, Interval } from './apiService';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import { DateTimePicker } from '@mui/lab';
 import AdapterJalali from '@date-io/date-fns-jalali';
+import useDelayedState from './useDelayedState';
 
 type IntervalsFilterProps = {
   // intervals: Interval[],
@@ -22,10 +23,11 @@ type IntervalsFilterProps = {
   },
   onChange(name: 'start' | 'end', value: Date | null): void,
   onChange(name: 'selectedActivities', value: Activity[]): void,
+  onReset(): void,
 };
 
 export default function IntervalsFilter({
-  activities, state, results: { sum }, onChange,
+  activities, state, results: { sum }, onChange, onReset: handleReset,
 }: IntervalsFilterProps) {
   const theme = useTheme();
   return (
@@ -37,7 +39,7 @@ export default function IntervalsFilter({
         <Box>
           <b>sum:</b> {sum >= 1440 && Math.floor(sum / 1440).toString() + ' days and '} {
             Math.floor((sum % 1440) / 60).toString().padStart(2, '0') +
-            ':' + (sum % 60).toString().padStart(2, '0')
+            ':' + Math.floor(sum % 60).toString().padStart(2, '0')
           }
         </Box>
       </Box>
@@ -81,6 +83,11 @@ export default function IntervalsFilter({
               )}
             />
           )}
+          <div>
+            <Button onClick={handleReset}>
+              reset filters
+            </Button>
+          </div>
         </Stack>
       </LocalizationProvider>
     </Paper>
@@ -103,17 +110,20 @@ type useIntervalsFilterProps = {
 };
 
 export function useIntervalsFilter({ intervals }: useIntervalsFilterProps) {
-  const [state, setState] = useState(() => {
+  const initState = useMemo(() => {
     let min = new Date().toISOString();
-    intervals.forEach(i => {
-      if (i.start < min) min = i.start;
-    });
+    intervals.forEach(i => { if (i.start < min) min = i.start; });
     return {
       start: new Date(min),
       end: intervals[0]?.end ? new Date(intervals[0].end) : new Date(),
       selectedActivities: [] as Activity[],
     };
-  });
+  }, [intervals]);
+  const [state, setState, delayedSate] = useDelayedState(initState);
+
+  const onReset = () => {
+    setState(initState);
+  };
 
   const onChange: IntervalsFilterProps['onChange'] = (name, value) => {
     setState(prev => ({ ...prev, [name]: value }));
@@ -121,18 +131,18 @@ export function useIntervalsFilter({ intervals }: useIntervalsFilterProps) {
 
   const filteredIntervals = useMemo(() => {
     const timeFiltered = intervals.filter(i => (
-      (new Date(i.start) <= state.end && new Date(i.start) >= state.start) ||
-      (new Date(i.end) <= state.end && new Date(i.end) >= state.start)
+      (new Date(i.start) <= delayedSate.end && new Date(i.start) >= delayedSate.start) ||
+      (new Date(i.end) <= delayedSate.end && new Date(i.end) >= delayedSate.start)
     ));
-    if (state.selectedActivities.length === 0) return timeFiltered;
+    if (delayedSate.selectedActivities.length === 0) return timeFiltered;
     return timeFiltered.filter(i => (
-      i.entries.map(e => e.activity).some(a => state.selectedActivities.some(b => isChildOf(a, b)))
+      i.entries.map(e => e.activity).some(a => delayedSate.selectedActivities.some(b => isChildOf(a, b)))
     ));
-  }, [state, intervals]);
+  }, [delayedSate, intervals]);
 
   const results = useMemo(() => ({
     sum: filteredIntervals.reduce((a, v) => (new Date(v.end).getTime() - new Date(v.start).getTime()) / 60000 + a, 0),
   }), [filteredIntervals]);
 
-  return { state, onChange, filteredIntervals, results };
+  return { state, onChange, filteredIntervals, results, onReset };
 }
