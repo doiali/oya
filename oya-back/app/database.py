dd012e829ad932f40ebe5a08ff952b37ab57452e
abagheri1994 <abagheri1994@gmail.com>
2022-01-11 20:49:31 +0330

    back: using future flag for db engine

--- oya-back/app/database.py
+++ oya-back/app/database.py
@@ -4,10 +4,15 @@ from sqlalchemy.orm import sessionmaker
 from decouple import config
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./oya.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://" + config('db_username') + ":" + \
-                          config('db_password') + "@localhost/oya"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://"
+    + config("db_username")
+    + ":"
+    + config("db_password")
+    + "@localhost/oya"
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, future=True)
 # engine = create_engine(
 #     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": True}
 # )
