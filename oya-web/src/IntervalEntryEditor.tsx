0d5afe01ae0cfe80bc02b5676185380aa46168fe
pooria <abagheri1994@gmail.com>
2022-01-22 22:56:14 +0330

    added IntervalEntryEditor

---
+++ oya-web/src/IntervalEntryEditor.tsx
@@ -0,0 +1,67 @@
+import { Chip, IconButton, Stack, TextField, Box } from '@mui/material';
+import { memo } from 'react';
+import { Entry } from './apiService/types';
+import useActivities from './useActivities';
+import { Delete } from '@mui/icons-material';
+
+type EntryEditorProps = {
+  entry: Entry,
+  intervalDuration: number,
+  onChange(e: Entry): void,
+  onDelete(e: Entry): void,
+};
+
+export default memo(function EntryEditor({
+  entry, intervalDuration, onChange, onDelete,
+}: EntryEditorProps) {
+  const { activityMappings } = useActivities();
+  return (
+    <Stack direction="row" spacing={2} sx={{ alignItems: 'center' }}>
+      <Box sx={{ minWidth: '25%' }}>
+        <Chip label={activityMappings[entry.activity_id]?.name} />
+        <span>{' '}:{' '}</span>
+      </Box>
+      <TextField
+        variant="standard"
+        required
+        sx={{ minWidth: 170 }}
+        inputProps={{
+          min: 0,
+          max: intervalDuration,
+        }}
+        value={entry.time ? entry.time / 60 : ''}
+        type="number"
+        label="spent time (minutes)"
+        onChange={e => onChange({ ...entry, time: Math.round(Number(e.target.value)) * 60 })}
+      />
+      <TextField
+        inputProps={{
+          min: 0,
+          max: 100,
+          maxLength: 5,
+        }}
+        sx={{ maxWidth: 100 }}
+        disabled
+        variant="standard"
+        value={entry.time ? Math.round(entry.time / 60 / intervalDuration * 100 * 100) / 100 : ''}
+        type="number"
+        label="%"
+        onChange={e => onChange({
+          ...entry,
+          time: (Number(e.target.value) / 100 * intervalDuration) * 60,
+        })}
+      />
+      <TextField
+        sx={{ flexGrow: 1 }}
+        variant="standard"
+        value={entry.note ?? ''}
+        type="text"
+        label="note"
+        onChange={e => onChange({ ...entry, note: e.target.value })}
+      />
+      <IconButton sx={{ flexShrink: 0 }} onClick={() => onDelete(entry)}>
+        <Delete />
+      </IconButton>
+    </Stack>
+  );
+});
--- oya-web/src/IntervalForm.tsx
+++ oya-web/src/IntervalForm.tsx
@@ -1,12 +1,13 @@
 import AdapterJalali from '@date-io/date-fns-jalali';
-import { Box } from '@mui/system';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import { DateTimePicker } from '@mui/lab';
-import { Autocomplete, Stack, TextField } from '@mui/material';
+import { Box, Autocomplete, Divider, Stack, TextField } from '@mui/material';
 import React, { memo } from 'react';
 import { Entry } from './apiService/types';
 import MarkdownBox from './MarkdownBox';
 import useActivities from './useActivities';
+import { getDeltaStringOfRange, getTimeDelta } from './utils';
+import IntervalEntryEditor from './IntervalEntryEditor';
 
 export type IntervalFormProps = {
   state: {
@@ -22,8 +23,38 @@ export type IntervalFormProps = {
   onChange(name: 'selectedEntries', value: Entry[]): void,
 };
 
-export default memo(function IntervalForm({ state, onChange, children, onSubmit: handleSubmit }: IntervalFormProps) {
+export default memo(function IntervalForm({
+  state, onChange, children, onSubmit: handleSubmit,
+}: IntervalFormProps) {
   const { activities, activityMappings } = useActivities();
+  const intervalDuration = getTimeDelta(state.start, state.end);
+  const entriesTotalTime = state.selectedEntries.reduce(
+    (a, v) => a + (v.time ?? intervalDuration * 60), 0,
+  ) / 60;
+  const handleAutoCompleteChange = (_: any, newVal: number[]) => {
+    const newEntries = [...state.selectedEntries.filter(e => newVal.includes(e.activity_id))];
+    newVal.forEach(val => {
+      if (!state.selectedEntries.some(e => e.activity_id === val)) {
+        newEntries.push({ activity_id: val, time: intervalDuration * 60, note: '' });
+      }
+    });
+    onChange('selectedEntries', newEntries);
+  };
+
+  const handleEntryChange = (newEntry: Entry) => {
+    const newEntries = [...state.selectedEntries];
+    const i = newEntries.findIndex(e => e.activity_id === newEntry.activity_id);
+    if (i > -1) newEntries[i] = newEntry;
+    onChange('selectedEntries', newEntries);
+  };
+
+  const handleEntryDelete = (deletedEntry: Entry) => {
+    const newEntries = [...state.selectedEntries];
+    const i = newEntries.findIndex(e => e.activity_id === deletedEntry.activity_id);
+    if (i > -1) newEntries.splice(i, 1);
+    onChange('selectedEntries', newEntries);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <Box p={2}>
@@ -47,25 +78,50 @@ export default memo(function IntervalForm({ state, onChange, children, onSubmit:
                 onChange={(newValue) => onChange('end', newValue)}
                 label="end"
                 renderInput={(params) => <TextField fullWidth {...params} />}
-                minDateTime={state.start}
+                minDateTime={new Date(state.start.getTime() + 1)}
               />
             </Stack>
+            <span>
+              Interval duration: <b>{getDeltaStringOfRange(intervalDuration)}</b>
+              {' '} - {' '}
+              Entries total time: <b>{getDeltaStringOfRange(entriesTotalTime)}</b>
+              {' '} - {' '}
+              entries cover <b>{Math.round(entriesTotalTime / intervalDuration * 100)} %</b> of total time
+            </span>
             {activities && (
               <Autocomplete
                 multiple
                 options={activities.map(a => a.id)}
                 getOptionLabel={(o) => activityMappings[o]?.name ?? 'unkown activity'}
                 value={state.selectedEntries.map(e => e.activity_id)}
-                onChange={(_, newVal) => onChange('selectedEntries', newVal.map(x => ({ activity_id: x })))}
+                onChange={handleAutoCompleteChange}
                 renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    variant="outlined"
-                    label="Activities"
-                  />
+                  <>
+                    <input hidden required={state.selectedEntries.length === 0} />
+                    <TextField
+                      {...params}
+                      InputLabelProps={{
+                        required: true,
+                      }}
+                      required={state.selectedEntries.length === 0}
+                      variant="outlined"
+                      label="Activities"
+                    />
+                  </>
                 )}
               />
             )}
+            <Stack direction="column" spacing={1} divider={<Divider flexItem />}>
+              {state.selectedEntries.map(e => (
+                <IntervalEntryEditor
+                  key={e.activity_id}
+                  intervalDuration={intervalDuration}
+                  entry={e}
+                  onChange={handleEntryChange}
+                  onDelete={handleEntryDelete}
+                />
+              ))}
+            </Stack>
             <MarkdownBox
               name="notes"
               label="notes"
