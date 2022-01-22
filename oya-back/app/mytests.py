d6f988118087954f189663fbf6f0781f3bd3ec92
pooria <abagheri1994@gmail.com>
2022-01-22 19:15:21 +0330

    back: mytests: query for default time of entry.time

--- oya-back/app/mytests.py
+++ oya-back/app/mytests.py
@@ -1,23 +1,20 @@
 from typing import List
 
 from sqlalchemy.orm import Session
-from sqlalchemy import Column
-from . import crud, models, schemas
+from sqlalchemy import Column, select
+from . import crud, schemas
+from .models import Interval, Activity, Association, Entry
 from .database import SessionLocal, engine
 import datetime
 from dateutil import parser
 import copy
 
-models.Base.metadata.create_all(bind=engine)
-db: Session = SessionLocal()
-x0 = parser.parse('2022-01-13T13:30:00+03:30')
-x1 = datetime.datetime(year=2022, month=1, day=14,hour=14)
-x2 = x1 + datetime.timedelta(days=1)
-print(type(x0.time()))
-crud.get_daily_report(db=db,date=x0.date())
-# for a in res:
-#     print(a)
-#     # print(a.start_datetime, " " , a.end_datetime, a.entries)
-#     # for x in a:
-#     #     print(x,end=' --- ')
-#     # print("")
+with SessionLocal() as session:
+    """"""
+    stmt = select(Entry).where(Entry.time.is_(None)).order_by(Entry.interval_id)
+    res = session.execute(stmt).scalars().all()
+    for entry in res:
+        entry.time = entry.interval.end - entry.interval.start
+    for r in res:
+        print(r.interval_id, r.activity_id, r.time)
+    # session.commit()
