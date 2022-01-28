b2703ad1802622996c5abe3b265f7e450d7a766a
pooria <abagheri1994@gmail.com>
2022-01-28 18:44:22 +0330

    added all CRUD to ActivityPage

--- oya-web/src/ActivityPage.tsx
+++ oya-web/src/ActivityPage.tsx
@@ -1,58 +1,141 @@
 import { ChevronRight, ExpandMore } from '@mui/icons-material';
 import { TreeItem, TreeView } from '@mui/lab';
-import { Grid } from '@mui/material';
+import { Box, Button, Grid, Paper, Typography } from '@mui/material';
+import React, { useState } from 'react';
 import { Activity } from './apiService';
+import ActivityEditor from './ActivityEditor';
 import useActivities from './useActivities';
+import ActivityAdder from './ActivityAdder';
 
 export default function ActivityPage() {
   return (
+    <ActivityView />
+  );
+}
+
+// type ActivityViewProps = {
+//   activities?: Activity[];
+// };
+
+function ActivityView() {
+  const { activities, activityMappings } = useActivities();
+  const [expanded, setExpanded] = useState<string[]>([]);
+  const [selected, setSelected] = useState<string>('');
+  const selectedIds = selected.split('-');
+  const selectedActivityId = selectedIds[selectedIds.length - 1];
+  const selectedActivity = activityMappings[selectedActivityId];
+
+  const getAllNodeIds = () => {
+    const ids: string[] = [];
+    const getTreeIds = (activity: Activity, rendererId = '') => {
+      const prefix = rendererId ? (rendererId + '-') : '';
+      ids.push(prefix + activity.id);
+      activity.children.forEach(a => { getTreeIds(a, prefix + activity.id); });
+    };
+    activities.forEach(a => { getTreeIds(a); });
+    return ids;
+  };
+
+  const handleToggle = (event: React.SyntheticEvent, nodeIds: string[]) => {
+    setExpanded(nodeIds);
+  };
+
+  const handleSelect = (event: React.SyntheticEvent, nodeId: string) => {
+    setSelected(nodeId);
+  };
+
+  const handleExpandClick = () => {
+    setExpanded(oldExpanded => {
+      return oldExpanded.length === 0 ? getAllNodeIds() : [];
+    });
+  };
+
+  const filteredActivities = (
+    [...activities].filter(a => a.parents.length === 0)
+      .sort((a, b) => b.allChildIds.length - a.allChildIds.length)
+  );
+
+  return (
     <Grid container spacing={2}>
-      <Grid item xs={12} md={7}>
-        <ActivityTree />
+      <Grid item xs={12} md={6}>
+        <Box>
+          <Box sx={{ mb: 1 }}>
+            <Button onClick={handleExpandClick}>
+              {expanded.length === 0 ? 'Expand all' : 'Collapse all'}
+            </Button>
+          </Box>
+          <TreeView
+            expanded={expanded}
+            selected={selected}
+            onNodeToggle={handleToggle}
+            onNodeSelect={handleSelect}
+            defaultCollapseIcon={<ExpandMore />}
+            defaultExpandIcon={<ChevronRight />}
+          >
+            {filteredActivities.map((a) => (
+              <ActivityTreeSingle
+                type="children"
+                key={a.id}
+                activity={a}
+              />
+            ))}
+          </TreeView>
+        </Box>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Box
+          sx={{
+            position: 'fixed',
+            right: 0,
+            height: '100%',
+            overflowY: 'auto',
+            p: 2,
+            width: 600,
+          }}
+        >
+          <Paper elevation={2} sx={{ p: 2, mt: -2 }}>
+            <Typography variant='h5'>
+              Create Activity
+            </Typography>
+            {activities && <ActivityAdder activities={activities} />}
+          </Paper>
+          {selectedActivity && (
+            <Paper elevation={2} sx={{ p: 2, mt: 2 }}>
+              <Typography variant='h5'>
+                Update Activity
+              </Typography>
+              <ActivityEditor
+                activities={activities}
+                activity={selectedActivity}
+                onClose={() => setSelected('')}
+              />
+            </Paper>
+          )}
+        </Box>
       </Grid>
     </Grid>
   );
 }
 
-function ActivityTree() {
-  const { activities } = useActivities();
-  return (
-    <TreeView
-      defaultCollapseIcon={<ExpandMore />}
-      defaultExpandIcon={<ChevronRight />}
-    >
-      {activities.filter(a => a.parentIds.length === 0).map((a, i) => (
-        <ActivityTreeSingle
-          index={i + 1}
-          key={a.id}
-          rendererId={0}
-          activity={a}
-        />
-      ))}
-    </TreeView>
-  );
-}
-
 type ActivityTreeSingleProps = {
   activity: Activity;
-  rendererId: number;
-  type?: 'children' | 'parents';
-  index?: number;
+  rendererId?: string;
+  type?: 'children' | 'parents' | 'single';
 };
 
-function ActivityTreeSingle({ activity, rendererId, type = 'children', index }: ActivityTreeSingleProps) {
+function ActivityTreeSingle({ activity, rendererId = '', type = 'children' }: ActivityTreeSingleProps) {
+  const prefix = rendererId ? (rendererId + '-') : '';
   return (
     <TreeItem
-      nodeId={rendererId.toString + '-' + activity.id}
-      label={(index ? (index.toString().padStart(3, '') + ' - ') : '') + activity.name}
+      nodeId={prefix + activity.id}
+      label={activity.name}
     >
-      {activity[type].map((c, i) => (
+      {(type === 'children' || type === 'parents') && activity[type].map((c) => (
         <ActivityTreeSingle
           type={type}
-          index={i + 1}
           key={c.id}
           activity={c}
-          rendererId={activity.id}
+          rendererId={prefix + activity.id}
         />
       ))}
     </TreeItem>
