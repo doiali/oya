539956ea90ad8c0bc4f4741c3e865d2df9f931fb
pooria <abagheri1994@gmail.com>
2022-01-30 06:28:25 +0330

    added pagination to IntervalsList

--- oya-web/src/IntervalsList.tsx
+++ oya-web/src/IntervalsList.tsx
@@ -1,7 +1,7 @@
 import { Interval } from './apiService/types';
 import { format } from 'date-fns-jalali';
-import { Stack, Divider, Chip, Box, Typography } from '@mui/material';
-import React, { memo, useMemo } from 'react';
+import { Stack, Divider, Chip, Box, Typography, Pagination } from '@mui/material';
+import React, { memo, useEffect, useMemo, useState } from 'react';
 import IntervalsFilter, { useIntervalsFilter } from './IntervalsFilter';
 import IntervalItem from './IntervalItem';
 
@@ -9,40 +9,61 @@ type IntervalsListProps = {
   intervals: Interval[],
 };
 
+const rowsPerPage = 50;
 export default memo(function IntervalsList({ intervals }: IntervalsListProps) {
   const { filteredIntervals, ...intervalsFilterProps } = useIntervalsFilter({ intervals });
-
+  const rows = filteredIntervals.length;
+  const [page, setPage] = useState(1);
+  const totalPages = Math.ceil(rows / rowsPerPage) || 1;
+  useEffect(() => { setPage(1); }, [rows]);
   const list = useMemo(() => {
     let prevEnd = new Date('2050-1-1');
     return (
       <Stack spacing={1}>
-        {filteredIntervals.map((interval, i) => {
-          const end = new Date(interval.end);
-          end.setHours(0, 0, 0, 0);
-          const isNewDay = end < prevEnd;
-          if (isNewDay) prevEnd = end;
-          return (
-            <React.Fragment key={interval.id}>
-              {isNewDay && (
-                <Divider orientation="horizontal" flexItem>
-                  <Chip color="secondary" dir="rtl" label={format(new Date(interval.end), 'eeee d MMMM y')} />
-                </Divider>
-              )}
-              <IntervalItem index={filteredIntervals.length - i} interval={interval} />
-            </React.Fragment>
-          );
-        })}
+        {filteredIntervals
+          .slice((page - 1) * rowsPerPage, page * rowsPerPage)
+          .map((interval, i) => {
+            const end = new Date(interval.end);
+            end.setHours(0, 0, 0, 0);
+            const isNewDay = end < prevEnd;
+            if (isNewDay) prevEnd = end;
+            return (
+              <React.Fragment key={interval.id}>
+                {isNewDay && (
+                  <Divider orientation="horizontal" flexItem>
+                    <Chip color="secondary" dir="rtl" label={format(new Date(interval.end), 'eeee d MMMM y')} />
+                  </Divider>
+                )}
+                <IntervalItem index={rows - (page - 1) * rowsPerPage - i} interval={interval} />
+              </React.Fragment>
+            );
+          })}
       </Stack>
     );
-  }, [filteredIntervals]);
-
+  }, [filteredIntervals, rows, page]);
+  const pagination = (rows > rowsPerPage) && (
+    <Stack spacing={1} direction="row"
+      sx={{ alignItems: 'center', justifyContent: 'space-between' }}
+    >
+      <span>
+        total rows: {rows}
+      </span>
+      <Pagination
+        count={totalPages}
+        page={page}
+        onChange={(_, x) => setPage(x)}
+      />
+    </Stack>
+  );
   return (
     <Box component="section">
       <IntervalsFilter {...intervalsFilterProps} />
       <Typography mb={2} variant="h5">
         Intervals List
       </Typography>
+      {pagination}
       {list}
+      {(rows > 10) && pagination}
     </Box>
   );
 });
