61f1cc41efd0f5c8d7d6067cb120f62e2fc07160
pooria <abagheri1994@gmail.com>
2022-02-06 19:37:52 +0330

    added time report

---
+++ oya-web/src/report/TimeRe.tsx
@@ -0,0 +1,42 @@
+import { Box, Typography } from '@mui/material';
+import {
+  Brush, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Bar, ComposedChart,
+} from 'recharts';
+import { useReportContext } from './ReportProvider';
+import { format } from 'date-fns-jalali';
+import { Activity } from '../apiService';
+
+export default function TimeRe({ activity }: { activity: Activity; }) {
+  const { dda } = useReportContext();
+  const data = dda.map(d => {
+    return {
+      name: format(d.date, 'MM/dd'),
+      value: Math.round(d.report[activity.id]?.time ?? 0),
+    };
+  });
+  return (
+    <Box sx={{ width: '100%', height: 600, maxWidth: 1700 }}>
+      <Typography align='center' variant='h5'>{activity.name}</Typography>
+      <ResponsiveContainer width="100%" height="100%">
+        <ComposedChart
+          width={500}
+          height={200}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Bar fill="#8884d8" dataKey="value" />
+          <Brush />
+        </ComposedChart>
+      </ResponsiveContainer>
+    </Box>
+  );
+}
---
+++ oya-web/src/report/TimeReport.tsx
@@ -0,0 +1,32 @@
+import { Grid } from '@mui/material';
+import { useMemo, useState } from 'react';
+import useActivities from '../useActivities';
+import ActivitiesTreeView from '../ActivitiesTreeView';
+import ReportProvider from '../report/ReportProvider';
+import TimeRe from './TimeRe';
+
+export default function TimeReport() {
+  const [selectedNodeId, setSelectedNodeId] = useState<string>('');
+  const { activityMappings } = useActivities();
+  const selectedActivity = useMemo(() => {
+    const selectedIds = selectedNodeId.split('-');
+    const selectedActivityId = selectedIds[selectedIds.length - 1];
+    return activityMappings[selectedActivityId];
+  }, [selectedNodeId, activityMappings]);
+  const handleSelect = (nodeId: string) => { setSelectedNodeId(nodeId); };
+  return (
+    <ReportProvider>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={5} sx={{ order: { xs: 2, md: 1 } }}>
+          <ActivitiesTreeView
+            selected={selectedNodeId}
+            onNodeSelect={handleSelect}
+          />
+        </Grid>
+        <Grid item xs={12} md={7} sx={{ order: { xs: 1, md: 2 } }}>
+          {selectedActivity && <TimeRe activity={selectedActivity} />}
+        </Grid>
+      </Grid>
+    </ReportProvider>
+  );
+}
