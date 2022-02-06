771280b62187233b6b48b2a26ea9d1d542eaf1a4
pooria <abagheri1994@gmail.com>
2022-02-06 05:06:19 +0330

    sunburst: added detail functionality

--- oya-web/src/report/SunburstNivo.tsx
+++ oya-web/src/report/SunburstNivo.tsx
@@ -1,27 +1,44 @@
 import { ResponsiveSunburst } from '@nivo/sunburst';
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { useReportContext } from './ReportProvider';
 import { generateTreeDataNivo } from './chartUtils';
+import { useEffect, useMemo, useState } from 'react';
 
 export default function SunburstNivo() {
   const { atrm, activities } = useReportContext();
-  const data = generateTreeDataNivo(atrm, activities);
+  const originalData = useMemo(() => generateTreeDataNivo(atrm, activities), [atrm, activities]);
+  const [data, setData] = useState(originalData);
+  useEffect(() => {
+    setData(originalData);
+  }, [originalData]);
+
   return (
     <Box sx={{ width: '100%', height: 700, maxWidth: 1700 }}>
+      <Button disabled={originalData === data} onClick={() => setData(originalData)}>
+        reset
+      </Button>
       <ResponsiveSunburst
+        onClick={(datum) => {
+          setData(datum.data);
+        }}
         data={data}
         margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
         id="name"
         value="value"
         cornerRadius={2}
-        borderColor={{ theme: 'background' }}
+        borderColor={{
+          from: 'color', modifiers: [[
+            'darker',
+            1.5,
+          ]],
+        }}
         colors={{ scheme: 'nivo' }}
         childColor={{
           from: 'color',
           modifiers: [
             [
               'brighter',
-              0.1,
+              0.2,
             ],
           ],
         }}
@@ -32,7 +49,7 @@ export default function SunburstNivo() {
           modifiers: [
             [
               'darker',
-              1.4,
+              1.5,
             ],
           ],
         }}
