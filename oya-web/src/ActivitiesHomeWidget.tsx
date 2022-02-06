d398219b73951153f8dc0e575a346b897553ef10
pooria <abagheri1994@gmail.com>
2022-02-06 04:31:55 +0330

    added show more button to activitiesWidget

--- oya-web/src/ActivitiesHomeWidget.tsx
+++ oya-web/src/ActivitiesHomeWidget.tsx
@@ -1,5 +1,5 @@
 import { Edit, Search } from '@mui/icons-material';
-import { IconButton, Stack, Divider, TextField, InputAdornment, Collapse, useTheme, emphasize, Typography } from '@mui/material';
+import { IconButton, Stack, Divider, TextField, InputAdornment, Collapse, useTheme, emphasize, Typography, Button } from '@mui/material';
 import { Box } from '@mui/system';
 import { Activity } from './apiService';
 import { memo, useState } from 'react';
@@ -10,15 +10,18 @@ import useActivities from './useActivities';
 export function getActivityParentsNames(a: Activity): string {
   return a.name.toLowerCase() + ' ' + a.allParents.map((p) => p.name).join(' ');
 }
+const maxRows = 40;
 
 const ActivitiesHomeWidget = memo(function ActivitiesWidget() {
   const { activities } = useActivities();
   const [searchVal, setSearchVal] = useState('');
+  const [more, setMore] = useState(false);
   const theme = useTheme();
   const searchIndex = activities.map((a) => getActivityParentsNames(a));
   const filteredActivities: Activity[] = activities.filter((a, i) => {
     return searchIndex[i].includes(searchVal.toLowerCase());
   });
+  const rows = filteredActivities.length;
   return (
     <Box sx={{ p: 2, backgroundColor: emphasize(theme.palette.background.paper, 0.05) }} component="section">
       <Typography variant='h5'>
@@ -44,12 +47,17 @@ const ActivitiesHomeWidget = memo(function ActivitiesWidget() {
         }}
       />
       <Stack spacing={1} divider={<Divider orientation="horizontal" flexItem />}>
-        {filteredActivities.map((a) => (
+        {filteredActivities.slice(0, more ? undefined : maxRows).map((a) => (
           <ActivitySingle
             key={a.id}
             activity={a}
           />
         ))}
+        {rows > maxRows && (
+          <Button onClick={() => setMore(p => !p)}>
+            {more ? 'show less' : 'show more'}
+          </Button>
+        )}
       </Stack>
     </Box>
   );
