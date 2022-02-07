22820277d79ed93cd05bd9ed96551c2c339c5644
gp <gp@DESKTOP-BCNVME.none>
2022-02-07 15:07:02 +0330

    added a basic router to activities action panel

--- oya-web/src/ActivityPage.tsx
+++ oya-web/src/ActivityPage.tsx
@@ -1,4 +1,4 @@
-import { Grid, Paper, Stack, Typography } from '@mui/material';
+import { Box, Grid, Paper, Stack, Tab, Tabs, Typography } from '@mui/material';
 import { useMemo, useState } from 'react';
 import { Activity } from './apiService';
 import ActivityEditor from './ActivityEditor';
@@ -6,6 +6,9 @@ import useActivities from './useActivities';
 import ActivitiesTreeView from './ActivitiesTreeView';
 import ReportProvider from './report/ReportProvider';
 import ActivityOverviewReport from './report/ActivityOverviewReport';
+import { Route, Routes, useLocation } from 'react-router';
+import { Link } from 'react-router-dom';
+import TimeRe from './report/TimeRe';
 
 export default function ActivityPage() {
   const [selectedNodeId, setSelectedNodeId] = useState<string>('');
@@ -41,20 +44,58 @@ type ActivityActionPanelProps = {
   onClose: () => void,
 };
 
+const ActivityActionPanelHome = ({ activity, onClose }: ActivityActionPanelProps) => activity ? (
+  <Stack spacing={2}>
+    <Paper elevation={2} sx={{ p: 2, minWidth: 500 }}>
+      <Typography variant='h5' mb={2}>
+        Update Activity <span style={{ fontSize: '0.7em' }}>id: {activity.id}</span>
+      </Typography>
+      <ActivityEditor
+        activity={activity}
+        onClose={() => onClose()}
+      />
+    </Paper>
+    <ActivityOverviewReport activity={activity} />
+  </Stack>
+) : null;
+
+const PanelRoutes = [
+  {
+    link: 'overview',
+    label: 'overview',
+    element: ActivityActionPanelHome,
+  },
+  {
+    link: 'time',
+    label: 'time chart',
+    element: TimeRe,
+  },
+];
+
 function ActivityActionPanel({ activity, onClose }: ActivityActionPanelProps) {
-  if (!activity) return null;
+  const loc = useLocation();
+  const paths = loc.pathname.split('/');
+  const path = paths[paths.length - 1];
+  const allowed = PanelRoutes.map(r => r.link);
+
+  const current = allowed.indexOf(path);
   return (
-    <Stack spacing={2}>
-      <Paper elevation={2} sx={{ p: 2, minWidth: 500 }}>
-        <Typography variant='h5' mb={2}>
-          Update Activity <span style={{ fontSize: '0.7em' }}>id: {activity.id}</span>
-        </Typography>
-        <ActivityEditor
-          activity={activity}
-          onClose={() => onClose()}
-        />
-      </Paper>
-      <ActivityOverviewReport activity={activity} />
-    </Stack>
+    <>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={current === -1 ? 0 : current}>
+          {PanelRoutes.map(r => (
+            <Tab key={r.link} component={Link} to={r.link} label={r.label} />
+          ))}
+        </Tabs>
+      </Box>
+      <Box sx={{ py: 2 }}>
+        <Routes>
+          {PanelRoutes.map(({ link, element: C }) => (
+            <Route key={link} path={link} element={<C {...{ activity, onClose }} />} />
+          ))}
+          <Route path="*" element={<ActivityActionPanelHome {...{ activity, onClose }} />} />
+        </Routes>
+      </Box>
+    </>
   );
 }
