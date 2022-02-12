bdb51e31bfa7c5a5082348403f203e91f203502b
gp <gp@DESKTOP-BCNVME.none>
2022-02-12 17:10:38 +0330

    auth.py: removed extra code

--- oya-back/app/auth.py
+++ oya-back/app/auth.py
@@ -17,17 +17,6 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_DAYS = 30
 
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-
-
 class Token(BaseModel):
     access_token: str
     token_type: str
