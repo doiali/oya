73dca1f72c76058a3edcb36bf01fa036242ab2dc
gp <gp@DESKTOP-BCNVME.none>
2022-02-12 16:46:33 +0330

    added user manipulation apis in auth.py

--- oya-back/app/auth.py
+++ oya-back/app/auth.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
-from sqlalchemy import select
+from typing import List
+from sqlalchemy import false, select
 from sqlalchemy.orm import Session
 from fastapi import Depends, APIRouter, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -13,7 +14,7 @@ from .database import get_db
 # openssl rand -hex 32
 SECRET_KEY = "ec531cdaa34637c8e880a4e2adf632e90fa3b929fe514c01fab055902c882e06"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_DAYS = 30
 
 
 fake_users_db = {
@@ -36,15 +37,28 @@ class TokenData(BaseModel):
     username: str | None = None
 
 
-class User(BaseModel):
+class UserBase(BaseModel):
     username: str
     email: str | None = None
     firstname: str | None = None
-    lastname: bool | None = None
-    superuser: bool | None
-    
+    lastname: str | None = None
+    superuser: bool | None = None
+
     class Config:
-        orm_mode = True 
+        orm_mode = True
+
+
+class User(UserBase):
+    id: int
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(UserBase):
+    username: str | None
+    password: str | None
 
 
 class UserInDB(User):
@@ -114,24 +128,84 @@ async def get_current_user(
     return user
 
 
-@router.post("/token", response_model=Token)
-async def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
-):
-    user = authenticate_user(db, form_data.username, form_data.password)
+def create_token(db: Session, username: str, password: str):
+    user = authenticate_user(db, username, password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
 
+async def get_current_super_user(current_user: User = Depends(get_current_user)):
+    if not current_user.superuser:
+        raise HTTPException(status_code=403, detail="you don't have permission")
+    return current_user
+
+
+@router.post("/token", response_model=Token)
+async def form_login_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    return create_token(db, form_data.username, form_data.password)
+
+
 @router.get("/users/me/", response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_user)):
     return current_user
+
+
+@router.get("/users/", response_model=List[User])
+async def read_users(
+    current_user: User = Depends(get_current_super_user), db: Session = Depends(get_db)
+):
+    return db.execute(select(models.User)).scalars().all()
+
+
+@router.post("/users/", response_model=User)
+async def create_user(
+    data: UserCreate,
+    current_user: User = Depends(get_current_super_user),
+    db: Session = Depends(get_db),
+):
+    data.password = get_password_hash(data.password)
+    db_user = models.User(**data.dict())
+    db.add(db_user)
+    db.flush()
+    db.commit()
+    return db_user
+
+
+@router.put("/users/{user_id}/", response_model=User)
+async def update_user(
+    user_id: int,
+    data: UserUpdate,
+    current_user: User = Depends(get_current_super_user),
+    db: Session = Depends(get_db),
+):
+    if data.password:
+        data.password = get_password_hash(data.password)
+    data = data.dict()
+    db_user = db.get(models.User, user_id)
+    for key in data:
+        value = data[key]
+        if value is not None:
+            db_user.__setattr__(key, value)
+    db.commit()
+    return db_user
+
+@router.delete("/users/{user_id}/",status_code=204)
+async def delete_user(
+    user_id: int,
+    current_user: User = Depends(get_current_super_user),
+    db: Session = Depends(get_db),
+):
+    db_user = db.get(models.User, user_id)
+    db.delete(db_user)
+    db.commit()
