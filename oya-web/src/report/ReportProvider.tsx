121adb484386edc1c855f9716c58f712a33ec50d
gp <gp@DESKTOP-BCNVME.none>
2022-02-26 16:52:03 +0330

    added range logic to report provider

--- oya-web/src/report/ReportProvider.tsx
+++ oya-web/src/report/ReportProvider.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useMemo, useState } from 'react';
-import { Activity, Interval } from '../apiService';
+import { Activity, Interval, IntervalsMeta } from '../apiService';
 import useActivities, { ActivityMappings } from '../useActivities';
 import {
   ActivityTotalReportMap,
@@ -13,62 +13,101 @@ import useIntervals from '../useIntervals';
 import { useOutletContext } from 'react-router';
 
 export type ReportContextState = {
-  startDate: Date | null;
-  endDate: Date | null;
+  start: Date | null;
+  end: Date | null;
 };
 
-export type ReportContext = {
+export type ReportContextBase = {
+  intervals: Interval[];
+  activities: Activity[];
+  activityMappings: ActivityMappings;
+  intervalsMeta?: IntervalsMeta;
   ATRM: ActivityTotalReportMap;
   ATRA: ActivityTotalReport[];
   DDM: DailyDataMap;
   DDA: DailyData[];
-  intervals: Interval[];
-  activities: Activity[];
-  activityMappings: ActivityMappings;
+};
+
+export type ReportContextResults = {
+  tATRM: ActivityTotalReportMap;
+  tATRA: ActivityTotalReport[];
+  tDDA: DailyData[];
   state: ReportContextState;
   onChange<T extends keyof ReportContextState>(name: T, value: ReportContextState[T]): void;
-};
+}
+
+export type ReportContext = ReportContextBase & ReportContextResults;
 
 const defaultValue: ReportContext = {
   ATRM: {},
   ATRA: [],
   DDM: {},
   DDA: [],
+  tATRM: {},
+  tATRA: [],
+  tDDA: [],
   intervals: [],
   activities: [],
   activityMappings: {},
   state: {
-    startDate: null,
-    endDate: null,
+    start: null,
+    end: null,
   },
   onChange: () => 0,
 };
 const ReportContext = createContext<ReportContext>(defaultValue);
 
+const getInitState = (meta: IntervalsMeta | undefined) => {
+  let start = new Date(0);
+  if (meta?.min)
+    start = new Date(meta.min);
+  start.setHours(0, 0, 0, 0);
+  let end = new Date();
+  if (meta?.max)
+    end = new Date(meta.max);
+  end.setDate(end.getDate() + 1);
+  end.setHours(0, 0, 0, 0);
+  return { start, end };
+};
+
 export function useReport(): ReportContext {
-  const { intervals } = useIntervals();
-  const { activityMappings, activities } = useActivities();
-  const [state, setState] = useState<ReportContextState>({
-    startDate: null,
-    endDate: null,
+  const { intervals, meta: intervalsMeta } = useIntervals({
+    onLoad: ({ meta }) => { setState(getInitState(meta)); },
   });
-  const value = useMemo<ReportContext>(() => {
+  const { activityMappings, activities } = useActivities();
+  const [state, setState] = useState<ReportContextState>(() => getInitState(intervalsMeta));
+  const base = useMemo<ReportContext>(() => {
     const onChange: ReportContext['onChange'] = (name, value) => {
       setState((prev) => ({ ...prev, [name]: value }));
     };
     const DDM = createDailyDataMap(intervals, activityMappings);
     const DDA = Object.values(DDM);
+    const tDDA = DDA.filter(({ date }) => {
+      if (!intervalsMeta) return true;
+      if (
+        !state.end || !state.start ||
+        isNaN(state.end.getTime()) || isNaN(state.start.getTime()) ||
+        (date <= state.end && date >= state.start)
+      ) {
+        return true;
+      }
+      return false;
+    });
     const ATRM = createActivityTotalReportMap(DDA);
     const ATRA = Object.values(ATRM).sort((a, b) => (
       Number(b?.time) - Number(a?.time)
     )) as ActivityTotalReport[];
+    const tATRM = createActivityTotalReportMap(tDDA);
+    const tATRA = Object.values(tATRM).sort((a, b) => (
+      Number(b?.time) - Number(a?.time)
+    )) as ActivityTotalReport[];
     return {
-      intervals, activityMappings,
-      activities, DDM, DDA, ATRM,
-      ATRA, state, onChange,
+      intervals, intervalsMeta, activityMappings, activities,
+      DDM, DDA, ATRM, ATRA, tDDA, tATRM, tATRA,
+      state, onChange,
     };
-  }, [intervals, activities, activityMappings, state]);
-  return value;
+  }, [intervals, activities, activityMappings, state, intervalsMeta]);
+  return base;
 }
 
 export default function ReportProvider({ children }: { children?: React.ReactNode; }) {
