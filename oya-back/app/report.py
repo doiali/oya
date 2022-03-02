4ff7ee2a78a4cdcc913a12369b056ad002babedd
pooria <abagheri1994@gmail.com>
2022-03-02 19:31:27 +0330

    added percent and trimmed time

--- oya-back/app/report.py
+++ oya-back/app/report.py
@@ -19,15 +19,17 @@ def get_intervals2(
     meta = db.execute(stmt_meta).first()
     min = from_date if from_date is not None else meta.min
     max = to_date if to_date is not None else meta.max
+
     t_end_temp = case((Interval.end > max, max), else_=Interval.end).label("t_end")
     t_start_temp = case((Interval.start < min, min), else_=Interval.start).label(
         "t_start"
     )
-    percent = (
+    percent_temp = (
         extract("EPOCH", (t_end_temp - t_start_temp))
         / extract("EPOCH", Interval.end - Interval.start)
     ).label("percent")
-    t_time = (percent * Entry.time).label("t_time")
+    t_time_temp = (percent_temp * Entry.time).label("t_time")
+
 
     Interval2 = aliased(Interval)
     Entry2 = aliased(Entry)
@@ -36,6 +38,11 @@ def get_intervals2(
     period_start = baseIndex * tick + min
     period_end = case((period_start + tick > max, max), else_=period_start + tick)
     t_end = case((Interval2.end > period_end, period_end), else_=Interval2.end)
+    percent_1 = (
+        extract("EPOCH", (t_end - t_start))
+        / extract("EPOCH", Interval2.end - Interval2.start)
+    )
+    t_time_1 = (percent_1 * Entry2.time)
     cte1_a = (
         select(
             Interval2.id.label("interval_id"),
@@ -48,31 +55,45 @@ def get_intervals2(
             period_end.label("period_end"),
             Interval2.start.label("interval_start"),
             Interval2.end.label("interval_end"),
+            percent_1.label("percent"),
+            t_time_1.label("t_time"),
+            Entry2.time.label("entry_time"),
         )
         .select_from(Entry2)
         .join(Interval2)
+        .where(Interval2.end >= min)
+        .where(Interval2.start <= max)
         .cte(recursive=True)
     )
     period_end2 = case(
         (cte1_a.c.period_start + tick * 2 > max, max),
         else_=cte1_a.c.period_start + tick * 2,
     )
+    t_start2 = (cte1_a.c.period_start + tick)
     t_end2 = case(
         (cte1_a.c.interval_end > period_end2, period_end2),
         else_=cte1_a.c.interval_end,
     )
+    percent_2 = (
+        extract("EPOCH", (t_end2 - t_start2))
+        / extract("EPOCH", cte1_a.c.interval_end - cte1_a.c.interval_start)
+    )
+    t_time_2 = (percent_2 * cte1_a.c.entry_time)
     cte1 = cte1_a.union_all(
         select(
             cte1_a.c.interval_id,
             cte1_a.c.activity_id,
             (cte1_a.c.index + 1).label("index"),
             (cte1_a.c.line + 1).label("line"),
-            (cte1_a.c.period_start + tick).label("t_start"),
+            t_start2.label("t_start"),
             t_end2.label("t_end"),
             (cte1_a.c.period_start + tick).label("period_start"),
             period_end2.label("period_end"),
             cte1_a.c.interval_start,
             cte1_a.c.interval_end,
+            percent_2.label("percent"),
+            t_time_2.label("t_time"),
+            cte1_a.c.entry_time,
         )
         .where(cte1_a.c.period_start + tick < cte1_a.c.interval_end)
         .where(cte1_a.c.period_start + tick < max)
@@ -90,6 +111,15 @@ def get_intervals2(
             cte1.c.activity_id,
             cte1.c.line,
             cte1.c.index,
+            cte1.c.interval_start,
+            cte1.c.interval_end,
+            cte1.c.t_start,
+            cte1.c.t_end,
+            cte1.c.period_start,
+            cte1.c.period_end,
+            cte1.c.entry_time,
+            cte1.c.t_time,
+            cte1.c.percent,
         )
         .select_from(cte1)
         .order_by(cte1.c.interval_id.desc(), cte1.c.activity_id, cte1.c.index.desc())
@@ -100,7 +130,7 @@ def get_intervals2(
             Entry.activity_id,
             func.count(Entry.interval_id).label("occurance"),
             func.sum(cte2.c.count).label("days"),
-            func.sum(t_time),
+            func.sum(t_time_temp),
         )
         .select_from(Entry)
         .join(Interval)
@@ -128,7 +158,7 @@ def get_intervals2(
     )
     entries_counter_result = db.execute(entries_counter).first()
     count = db.execute(stmt_counter).first().count
-    rows = db.execute(stmt.limit(1000)).all()
+    rows = db.execute(stmt.limit(50)).all()
     return {
         "meta": {
             "count": count,
